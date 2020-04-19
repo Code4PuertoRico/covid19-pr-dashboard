f53d6345be85ae18d7e1d40dc51aebd765eec8d4
pedrocruzio <pedro@rokailabs.com>
2020-04-19 15:38:40 -0400

    bug: renamed chart component

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30a4c7ce5db1001ad4b0a26aec4f8b10.js"
+  "/precache-manifest.602736058ea7dad7ccccf3ba93317520.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "covid19-pr-dashboard"});
